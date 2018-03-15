f00a7ddbae21d4c0d942b28994f8e0ae56793141
Patrick Tescher <pat2man@gmail.com>
2018-03-15 15:54:07 -0700

    Switch networks based on host

--- app/client/lib/ethereum/1_web3Init.js
+++ app/client/lib/ethereum/1_web3Init.js
@@ -1,6 +1,10 @@
 // set providor
 if (Meteor.isDevelopment) {
-  web3 = new Web3(bitski.InitializeWeb3('F3YKmUz8wJPevbjd0LJOfSTkg4IiwWlcypE6AdBXweui1lhjC1kcGDgBCub35QkO', 'http://localhost:3000/').currentProvider);
+  web3 = new Web3(bitski.InitializeWeb3('F3YKmUz8wJPevbjd0LJOfSTkg4IiwWlcypE6AdBXweui1lhjC1kcGDgBCub35QkO', 'kovan', 'http://localhost:3000/').currentProvider);
+} else if (window.location.host.match('kovan')) {
+  web3 = new Web3(bitski.InitializeWeb3('F3YKmUz8wJPevbjd0LJOfSTkg4IiwWlcypE6AdBXweui1lhjC1kcGDgBCub35QaC', 'kovan', 'https://kovan-wallet.bitski.com/').currentProvider);
+} else if (window.location.host.match('rinkeby')) {
+  web3 = new Web3(bitski.InitializeWeb3('F3YKmUz8wJPevbjd0LJOfSTkg4IiwWlcypE6AdBXweui1lhjC1kcGDgBCub35QaB', 'rinkeby', 'https://rinkeby-wallet.bitski.com/').currentProvider);
 } else {
-  web3 = new Web3(bitski.InitializeWeb3('F3YKmUz8wJPevbjd0LJOfSTkg4IiwWlcypE6AdBXweui1lhjC1kcGDgBCub35QkR', 'https://wallet.bitski.com/').currentProvider);
+  web3 = new Web3(bitski.InitializeWeb3('F3YKmUz8wJPevbjd0LJOfSTkg4IiwWlcypE6AdBXweui1lhjC1kcGDgBCub35QaA', 'mainnet', 'https://wallet.bitski.com/').currentProvider);
 }
